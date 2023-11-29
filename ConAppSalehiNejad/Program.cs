2dc08e3edf1741d2ce375d459177d66f06ec992e
donottouchmylaptop <tag@senioradministrator.com>
2023-11-29 10:30:02 +0330

    the function multiple has been written.

--- ConAppSalehiNejad/Program.cs
+++ ConAppSalehiNejad/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+void funcMultiple()
+{
+    Console.Write("please enter first number:");
+    int i = int.Parse(Console.ReadLine());
+    int j = int.Parse(Console.ReadLine());
+    //int k = i*j;
+}
